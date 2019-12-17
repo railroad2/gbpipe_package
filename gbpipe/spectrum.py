fa64513a8e6b349754bdc973f60c9ed93dc3d65e
Kyungmin Lee <kmlee@hep.korea.ac.kr>
2019-12-17 13:15:41 +0000

    correct import of xpol

--- gbpipe/spectrum.py
+++ gbpipe/spectrum.py
@@ -1,19 +1,13 @@
 import numpy as np
 import healpy as hp
 import camb
-<<<<<<< HEAD
-try:
-    import xpol
-except ModuleNotFoundError:
-    print ('xpol is not found')
 
-=======
->>>>>>> 788d76b2582145ca220472eb967fde46c3d326fd
 from .utils import dl2cl, print_warning
 
 try:
     import xpol
-except:
+except ModuleNotFoundError:
+    print ('xpol is not found. Using camb instead.')
     xpol = camb
 
 args_cosmology = ['H0', 'cosmomc_theta', 'ombh2', 'omch2', 'omk', 
